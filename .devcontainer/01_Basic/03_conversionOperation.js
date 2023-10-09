15db3fe594481fafd4d5fcbabf9f0bc8bc1509a7
shreyashgosavi <126142795+shreyashgosavi@users.noreply.github.com>
2023-10-09 11:49:00 +0000

    conversion js

---
+++ .devcontainer/01_Basic/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "23abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let thevalueInnumber = Number(score)
+
+console.log(typeof thevalueInnumber);
+console.log(thevalueInnumber);
+
+
+
+//conversion
+// 33 => 33
+// "33abcx" => NaN(Not a Number)
+// true => 1 / false => 0
+// null => 0
+// undefined => NaN
+
+
+let LoggedIn = "abc"
+let booleanLoggedIn = Boolean(LoggedIn)
+console.log(booleanLoggedIn);
+
+// 1 => true  / 0 => false
+//"" => false
+//"abc" => true
+
+let number= 50
+
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
