e6e334e148238e6e1f9efebb1f6b986b7efc1177
shreyashgosavi <126142795+shreyashgosavi@users.noreply.github.com>
2023-10-09 10:29:22 +0000

    dataTypes

---
+++ .devcontainer/01_Basic/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as new version
+
+// alert(3 + 3) // we are using nodejs not browser This works in browser
+
+let name = "shreyash"
+let age = 20
+let isLoggedIn = true
+
+// datatypes
+// number => range 2 to the power 53 or somthing around that 
+// bigint => for very big number size and range
+// string => "" /''
+// boolean => true/false 
+// null => stand alone value
+// undefined => not metioned
+// symbol => unqiue
+
+//object
+
+console.log(typeof "shreyash");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+
