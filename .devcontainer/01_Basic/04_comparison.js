0d222c7ea381a71c5965a45153f9be17f98aab24
shreyashgosavi <126142795+shreyashgosavi@users.noreply.github.com>
2023-10-09 15:17:46 +0000

    comparison in js

---
+++ .devcontainer/01_Basic/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(1 < 2);
+// console.log(1 > 2);
+// console.log(1 <= 2);
+// console.log(1 >= 2);
+// console.log(1 == 2);
+// console.log(1 != 2);
+
+// console.log("1" > 2);
+// console.log(1 < "2");
+
+//avoid this 
+// console.log(null < 0);
+// console.log(null <= 0);
+// console.log(null == 0);
+
+console.log(undefined < 0);
+console.log(undefined <= 0);
+console.log(undefined == 0);
+
+console.log("2" === 2);// this check also types
+
+
