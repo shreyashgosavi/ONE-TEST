2cd3fa1a077af38b59a3edfc4a43af1627ea804d
shreyashgosavi <126142795+shreyashgosavi@users.noreply.github.com>
2023-10-09 07:57:29 +0000

    one part done

---
+++ .devcontainer/01_Basic/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1215142
+let accountEmail = "abc.dc@gamil.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState;
+
+//if you define variable but not assgin value it take as undefined
+//accountId = 3 //this not vaild
+
+accountEmail = "shreyash.sh"
+accountPassword = 1122233
+accountCity = "pune"
+
+/*perfer not use var 
+because issuse in block scope and funtional scope
+*/
+
+console.table([accountEmail,accountId,accountCity,accountPassword,accountState])
+
